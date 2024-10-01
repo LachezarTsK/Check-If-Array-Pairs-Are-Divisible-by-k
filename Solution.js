aab67509b4947da076e47a086966a21cc4416ef7
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-10-01 18:42:42 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param {number[]} input
+ * @param {number} divisor
+ * @return {boolean}
+ */
+var canArrange = function (input, divisor) {
+    const frequency = new Array(divisor + 1).fill(0);
+    for (let value of input) {
+        const remainder = ((value % divisor) + divisor) % divisor;
+        ++frequency[remainder];
+    }
+
+    if (frequency[0] % 2 !== 0) {
+        return false;
+    }
+    for (let i = Math.floor(divisor / 2); i >= 1; --i) {
+        if (frequency[i] !== frequency[divisor - i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
